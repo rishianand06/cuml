ed6bc4b892265863e1f40173457c116b48b26d8c
rishianand06 <rishianand8@gmail.com>
2022-01-18 14:58:48 +0530

    Create pulse_oss_scan.bash

---
+++ pulse_oss_scan.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+conda create -n cuml_dev python=3.7 cuml=22.02 cudatoolkit=11.2 -c conda-forge -c rapidsai-nightly -c nvidia -y
+cd conda/environments/ 
+touch environment.yml
+cd ../..
+export DETECT_CONDA_PATH=/root/miniconda3/bin/conda
+export DETECT_CONDA_ENVIRONMENT_NAME=cudf_dev
+pulse --url https://nvidia-test.app.blackduck.com --api-token ${PULSE_API_TOKEN} --project ${PULSE_SCAN_PROJECT} --project-version ${PULSE_SCAN_PROJECT_VERSION} --vulnerability-report scanReport.json conda/environments/
