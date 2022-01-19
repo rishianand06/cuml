6af6fab42d5230c1d2defdd8b3e743a9bd7d7db1
rishianand06 <rishianand8@gmail.com>
2022-01-19 14:32:24 +0530

    Update pulse_oss_scan.bash

--- pulse_oss_scan.bash
+++ pulse_oss_scan.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo $PULSE_API_TOKEN
-/usr/miniconda3/bin/conda create -n cuml_dev python=3.7 cuml=22.02 cudatoolkit=11.2 -c conda-forge -c rapidsai-nightly -c nvidia -y
-cd conda/environments/ 
-touch environment.yml
+# /usr/miniconda3/bin/conda create -n cuml_dev python=3.7 cuml=22.02 cudatoolkit=11.2 -c conda-forge -c rapidsai-nightly -c nvidia -y
+cd conda/environments/
+mv cuml_dev_cuda11.5.yml environment.yml
+# touch environment.yml
 cd ../..
-# export DETECT_CONDA_PATH=/usr/miniconda3/bin/conda
-# export DETECT_CONDA_ENVIRONMENT_NAME=cuml_dev
+export DETECT_CONDA_PATH=/usr/miniconda3/bin/conda
+export DETECT_CONDA_ENVIRONMENT_NAME=cuml_dev
 export DETECT_PROJECT_NAME=$PULSE_SCAN_PROJECT
-export DETECT_PROJECT_VERSIOn_NAME=$PULSE_SCAN_PROJECT_VERSION
+export DETECT_PROJECT_VERSION_NAME=use_existing_env_file
 wget https://detect.synopsys.com/detect7.sh --no-check-certificate
 bash detect7.sh  --blackduck.url=https://nvidia-test.app.blackduck.com --blackduck.api.token=$PULSE_API_TOKEN --detect.source.path='conda/environments/'
 # pulse --url https://nvidia-test.app.blackduck.com --api-token $PULSE_API_TOKEN scan --vulnerability-report scanReport.json conda/environments/
